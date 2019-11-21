3b6c8dd0a5b979348244952e737d60d9c16c1822
Ľubomír Koprla <koprla.lubomir@gmail.com>
2019-11-21 16:00:30 +0100

    Update preprocessing for yoochoose

--- src/data/load_data_yoochoose.py
+++ src/data/load_data_yoochoose.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import scipy.sparse
 
 def load_clicks():
     return pd.read_csv("../data/raw/yoochoose-csv/yoochoose-clicks.csv",names=["visitorid","timestamp","itemid","category"])
@@ -8,3 +10,21 @@ def load_purchases():
 
 def load_test():
     return pd.read_csv("../data/raw/yoochoose-csv/yoochoose-test.csv",names=["visitorid","timestamp","itemid","category"])
+
+def save_processed(data_x,data_y):
+    np.savetxt("../data/processed/yoochoose/data_x.csv", np.asarray(data_x), delimiter=",")
+    np.savetxt("../data/processed/yoochoose/data_y.csv", np.asarray(data_y), delimiter=",")
+
+def load_processed():
+    data_x = np.genfromtxt("../data/processed/yoochoose/data_x.csv",delimiter=",")
+    data_y = np.genfromtxt("../data/processed/yoochoose/data_y.csv", delimiter=",")
+    return data_x,data_y
+
+def save_processed_sparse(data_x,data_y):
+    np.savetxt("../data/processed/yoochoose/data_x.csv", np.asarray(data_x), delimiter=",")
+    scipy.sparse.save_npz('../data/processed/yoochoose/data_y.npz', data_y)
+
+def load_processed_sparse():
+    data_x = np.genfromtxt("../data/processed/yoochoose/data_x.csv",delimiter=",")
+    data_y = scipy.sparse.load_npz("../data/processed/yoochoose/data_y.npz")
+    return data_x,data_y
