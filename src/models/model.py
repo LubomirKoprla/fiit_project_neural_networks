a3bae2200ff72a0db3d841f7ab94682859d972cd
blanarik <patrik.blanarik@gmail.com>
2019-11-22 00:05:23 +0100

    Model generalized

--- src/models/model.py
+++ src/models/model.py
@@ -1,28 +1,32 @@
-from keras.layers import LSTM, Dense, Embedding, Bidirectional
-import keras
-import tensorflow as tf    
-    
-class LSTMrecommender(keras.Model):
+from keras.layers import Embedding, LSTM, Dense
+from keras import Model
 
-    def __init__(self, emb_input_dim, emb_output_dim, lstm_units, dense_output_dim, dense_activation):
-        super(LSTMrecommender, self).__init__()
+
+class LSTMRec(Model):
+    def __init__(self, vocabulary_size, emb_output_dim, lstm_units,
+                 lstm_activation, lstm_recurrent_activation,
+                 lstm_dropout, lstm_recurrent_dropout,
+                 dense_activation
+                 ):
+        super(LSTMRec, self).__init__()
         self.emb = Embedding(
-            input_dim=emb_input_dim,
+            input_dim=vocabulary_size + 1,
             output_dim=emb_output_dim,
-            trainable=True,
             mask_zero=True
-        )  
-        
+        )
+
         self.lstm = LSTM(
             units=lstm_units,
-            return_sequences=False
+            activation=lstm_activation,
+            recurrent_activation=lstm_recurrent_activation,
+            dropout=lstm_dropout,
+            recurrent_dropout=lstm_recurrent_dropout
         )
 
         self.dense = Dense(
-            units=dense_output_dim, # how many outputs to produce = # of items
+            units=vocabulary_size,
             activation=dense_activation
         )
-        
 
     def call(self, x):
         mask = self.emb.compute_mask(x)
@@ -30,4 +34,3 @@ class LSTMrecommender(keras.Model):
         x = self.lstm(x, mask=mask)
         x = self.dense(x)
         return x
-    
